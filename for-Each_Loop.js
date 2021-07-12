ae1c3e3aa2e0ce637a61bd17e3e1a794891d1914
imsushant12 <sushantgaurav57@gmail.com>
2021-07-12 12:38:05 +0530

    first commit

---
+++ for-Each_Loop.js
@@ -0,0 +1,17 @@
+var arr = [
+    "hello",
+    "world",
+    "It's",
+    "Eight",
+    "PM"
+]
+
+// arr.forEach(i => console.log(i));
+// // Other way to Write : 
+// arr.forEach((i) => console.log(i));
+
+arr.forEach((i) => {
+    console.log(i);
+    let a = [1,2,3];
+    a.forEach((i) => console.log(i));
+})
