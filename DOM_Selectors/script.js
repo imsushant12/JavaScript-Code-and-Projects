444ed440334160e9aabca36e3fa560a72a772f30
imsushant12 <sushantgaurav57@gmail.com>
2021-07-12 12:36:54 +0530

    first commit

---
+++ DOM_Selectors/script.js
@@ -0,0 +1,5 @@
+var btn1 = document.getElementById("first");
+btn1.innerHTML = "Changed Due to ID : first through JS";
+
+var btn2 = document.getElementsByClassName("two");
+btn2[0].style.color = "red";
