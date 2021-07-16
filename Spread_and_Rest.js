45ec5dd538b3e2ed56a8d6dd36eaea279d243773
imsushant12 <sushantgaurav57@gmail.com>
2021-07-16 21:26:15 +0530

    first commit

---
+++ Spread_and_Rest.js
@@ -0,0 +1,26 @@
+function sumSpread(a, b) {
+  return a + b;
+}
+
+console.log("Using Normal Calling : " + sumSpread(10, 20));
+var value = [10, 20];
+console.log("Using SPREAD on same size of array : " + sumSpread(...value));
+var values = [1, 2, 3, 4, 5];
+// console.log(sumSpread(values));      // --> WRONG WAY
+console.log("Using SPREAD on varied size of array : " + sumSpread(...values));
+
+console.log("\n\n");
+
+function sumRest(...args) {
+  let sum = 0;
+  for (let i of args) sum += i;
+
+  return sum;
+}
+
+console.log("Using Normal Calling : " + sumRest(10, 20));
+var x = [10, 20];
+console.log("Using Rest on same size of array : " + sumRest(...x));
+var xArray = [1, 2, 3, 4, 5];
+// console.log(sumSpread(xArray));      // --> WRONG WAY
+console.log("Using Rest on varied size of array : " + sumRest(...xArray));
