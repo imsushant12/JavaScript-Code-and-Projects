9ff13829836482530546d40dce265ae45144a95e
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-01-08 20:22:22 +0530

    first commit

--- this_keyword.js
+++ this_keyword.js
@@ -1,17 +1,47 @@
-var count1 = 1;
-var count2 = 1;
+// var count1 = 1;
+// var count2 = 1;
 
-function counter() {
-    count1++;
-    this.count2++;
-    console.log(this);
-    
-    console.log(count1);
-    console.log(this.count1);
-    console.log(count2);
-    console.log(this.count2);
+// function counter() {
+//     count1++;
+//     this.count2++;
+//     console.log(this);
+
+//     console.log(count1);
+//     console.log(this.count1);
+
+//     console.log(count2);
+//     console.log(this.count2);
+// }
+
+// counter();
+// counter();
+
+function Car(name) {
+    this.name = name;
+    this.start = function() {
+        console.log(this.name);
+        console.log(this);
+    }
 }
 
-counter();
-counter();
+/*
+// Error because without new keyword, this is pointing to global object
+let swift = Car('Swift');
+swift.start();
+*/
 
+let swift = new Car('Swift');
+swift.start();
+
+
+function Car2(name) {
+    this.name = name;
+    this.start = function() {
+        console.log(this.name);
+        console.log(this);
+    }
+    return this;
+}
+
+let suv = Car2('Swift');
+suv.start();
